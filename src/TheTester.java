c3b0dd1d056199a2f6d43ec71e07a71d88d1940b
lazytosay <siweisijiao.weebly@gmail.com>
2020-02-11 16:58:20 -0800

    a test file, with simple example

--- src/TheTester.java
+++ src/TheTester.java
@@ -1,8 +1,24 @@
+import javax.swing.*;
 
 public class TheTester {
+	
 
 	public static void main(String[] args) {
-		System.out.println("this is the tester file for the project..");
+		//System.out.println("this is the tester file for the project..");
+		
+		//create a frame
+		JFrame f = new JFrame();
+		
+		//create a button
+		JButton b = new JButton("click");
+		b.setBounds(130, 100, 100, 40);
+		
+		f.add(b);
+		
+		f.setSize(100, 100);
+		f.setLayout(null);
+		f.setVisible(true);
+		
 
 	}
 
